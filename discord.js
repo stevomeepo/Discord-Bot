3376c8866d745607c130d15374ef2dfac0fab3a7
stevomeepo <stephenhuang8@gmail.com>
2024-01-24 16:11:22 -0800

    minor fixing

--- discord.js
+++ discord.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const { Client, GatewayIntentBits } = require('discord.js');
 const { joinVoiceChannel, createAudioPlayer, createAudioResource, entersState, VoiceConnectionStatus, AudioPlayerStatus } = require('@discordjs/voice');
 const ytdl = require('ytdl-core');
+const queues = new Map();
 const cookRegex = /c+o+o+k+/;
 const timeRegex = /t+i+m+e+/;
 const bogaRegex = /b+o+g+a+/;
@@ -33,8 +34,9 @@ let player;
 
 client.on('messageCreate', async message => {
   if (message.author.bot) return;
-
   const contentLower = message.content.toLowerCase();
+  const serverQueue = queues.get(message.guild.id);
+
   if (contentLower.startsWith('!play')) {
     const args = message.content.split(' ');
     if (args.length < 2) {
@@ -46,86 +48,108 @@ client.on('messageCreate', async message => {
       message.channel.send('Please provide a valid YouTube URL.');
       return;
     }
-    console.log("User's voice channel ID:", message.member.voice.channelId);
-    if (message.member.voice.channelId) {
-      const channel = message.guild.channels.cache.get(message.member.voice.channelId);
-      const connection = joinVoiceChannel({
-        channelId: channel.id,
-        guildId: message.guild.id,
-        adapterCreator: message.guild.voiceAdapterCreator,
-      });
+
+    if (!serverQueue) {
+      // Create a queue since one doesn't exist
+      const queue = {
+        textChannel: message.channel,
+        voiceChannel: message.member.voice.channel,
+        connection: null,
+        songs: [],
+        volume: 5,
+        playing: true,
+      };
+      queues.set(message.guild.id, queue);
+      queue.songs.push(youtubeURL);
+
       try {
-        await entersState(connection, VoiceConnectionStatus.Ready, 30e3);
-
-        player = createAudioPlayer();
-
-        const stream = ytdl(youtubeURL, { filter: 'audioonly' });
-        const resource = createAudioResource(stream);
-        player.play(resource);
-        connection.subscribe(player);
-        player.on(AudioPlayerStatus.Idle, () => connection.destroy());
-        player.on('error', error => console.error(`Error: ${error.message}`));
-        message.channel.send('Now playing your requested song!');
-      } catch (error) {
-        console.error(error);
-        message.channel.send('Failed to join your voice channel!');
-        connection.destroy();
+        const connection = joinVoiceChannel({
+          channelId: message.member.voice.channelId,
+          guildId: message.guild.id,
+          adapterCreator: message.guild.voiceAdapterCreator,
+        });
+        queue.connection = connection;
+        play(message.guild, queue.songs[0]);
+      } catch (err) {
+        console.error(err);
+        queues.delete(message.guild.id);
+        message.channel.send('Failed to join the voice channel.');
       }
     } else {
-      message.channel.send('You need to join a voice channel first!');
+      serverQueue.songs.push(youtubeURL);
+      message.channel.send(`Added to queue: ${youtubeURL}`);
     }
-  }
-
-  if (contentLower === '!pause') {
-    if (player) {
-      player.pause();
+  } else if (contentLower === '!stop') {
+    if (serverQueue) {
+      serverQueue.songs = [];
+      if (serverQueue.connection) {
+        serverQueue.connection.destroy();
+      }
+      queues.delete(message.guild.id);
+      message.channel.send('Stopped the music and cleared the queue.');
+    }
+  } else if (contentLower === '!pause') {
+    if (serverQueue && serverQueue.player) {
+      serverQueue.player.pause();
       message.channel.send('Paused the music.');
-    } else {
-      message.channel.send('No music is currently playing.');
     }
-  }
-
-  if (contentLower === '!resume') {
-    if (player) {
-      player.unpause();
+  } else if (contentLower === '!resume') {
+    if (serverQueue && serverQueue.player) {
+      serverQueue.player.unpause();
       message.channel.send('Resumed the music.');
-    } else {
-      message.channel.send('No music is currently paused.');
     }
   }
+});
 
-  if (contentLower === '!stop') {
-    if (player) {
-      player.stop();
-      message.channel.send('Stopped the music.');
-    } else {
-      message.channel.send('No music is currently playing.');
-    }
+function play(guild, song) {
+  const serverQueue = queues.get(guild.id);
+  if (!song) {
+    serverQueue.voiceChannel.leave();
+    queues.delete(guild.id);
+    return;
   }
 
-  if (bogaRegex.test(contentLower)) {
-    message.channel.send('Hello boga! I AM THE BOGA BOGA BOGA MONSTER');
-  } else if (message.content.toLowerCase().includes('good night') || message.content.toLowerCase().includes('gn')) {
-    message.channel.send('Good night bogas!');
-  } else if (cookRegex.test(contentLower) || timeRegex.test(contentLower)) {
-    message.channel.send("IT'S TIME TO COOK! @everyone");
-  } else if (goofyRegex.test(contentLower)) {
-    message.channel.send('Imma goofy goober!');
-  } else if (dummyRegex.test(contentLower)) {
-    message.channel.send('thats me hehe XD');
-  } else if (tickleRegex.test(contentLower)) {
-    message.channel.send("It's tickle tuesday!");
-  } else if (downRegex.test(contentLower)) {
-    message.channel.send("I'm acutally DOWNS");
-  } else if (lolRegex.test(contentLower)) {
-    message.channel.send("stop making me laugh so hard teehee");
-  } else if (lmaoRegex.test(contentLower)) {
-    message.channel.send("stop making me laugh so hard teehee");
-  } else if (mattRegex.test(contentLower)) {
-    message.channel.send("Matt is my boss");
-  } else if (poopRegex.test(contentLower)) {
-    message.channel.send("ayooo let me join");
-  } else if (dance1Regex.test(contentLower) || dance2Regex.test(contentLower)) {
-    message.channel.send("https://giphy.com/gifs/skeleton-dancing-tellmeohtellme-THlB4bsoSA0Cc");
-}});
+  const stream = ytdl(song, { filter: 'audioonly' });
+  const resource = createAudioResource(stream);
+  if (!serverQueue.player) {
+    serverQueue.player = createAudioPlayer();
+    serverQueue.connection.subscribe(serverQueue.player);
+  }
+  serverQueue.player.play(resource);
+
+  serverQueue.player.on(AudioPlayerStatus.Idle, () => {
+    serverQueue.songs.shift(); // Remove the finished song from the queue
+    play(guild, serverQueue.songs[0]); // Play the next song
+  });
+
+  serverQueue.player.on('error', error => console.error(`Error: ${error.message}`));
+  serverQueue.textChannel.send(`Now playing: ${song}`);
+}
+
+    if (bogaRegex.test(contentLower)) {
+      message.channel.send('Hello boga! I AM THE BOGA BOGA BOGA MONSTER');
+    } else if (message.content.toLowerCase().includes('good night') || message.content.toLowerCase().includes('gn')) {
+      message.channel.send('Good night bogas!');
+    } else if (cookRegex.test(contentLower) || timeRegex.test(contentLower)) {
+      message.channel.send("IT'S TIME TO COOK! @everyone");
+    } else if (goofyRegex.test(contentLower)) {
+      message.channel.send('Imma goofy goober!');
+    } else if (dummyRegex.test(contentLower)) {
+      message.channel.send('thats me hehe XD');
+    } else if (tickleRegex.test(contentLower)) {
+      message.channel.send("It's tickle tuesday!");
+    } else if (downRegex.test(contentLower)) {
+      message.channel.send("I'm acutally DOWNS");
+    } else if (lolRegex.test(contentLower)) {
+      message.channel.send("stop making me laugh so hard teehee");
+    } else if (lmaoRegex.test(contentLower)) {
+      message.channel.send("stop making me laugh so hard teehee");
+    } else if (mattRegex.test(contentLower)) {
+      message.channel.send("Matt is my boss");
+    } else if (poopRegex.test(contentLower)) {
+      message.channel.send("ayooo let me join");
+    } else if (dance1Regex.test(contentLower) || dance2Regex.test(contentLower)) {
+      message.channel.send("https://giphy.com/gifs/skeleton-dancing-tellmeohtellme-THlB4bsoSA0Cc");
+    }
+});
 client.login(process.env.DISCORD_TOKEN);

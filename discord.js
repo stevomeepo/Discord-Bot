c50780e6edefd5f3a2ff69b982a70963a75ceac2
stevomeepo <stephenhuang8@gmail.com>
2024-01-27 23:41:35 -0800

    fixing

--- discord.js
+++ discord.js
@@ -45,7 +45,7 @@ client.on('messageCreate', async message => {
   
     try {
       const response = await axios.post('https://api.openai.com/v1/chat/completions', {
-        model: "gpt-3.5-turbo",
+        model: "gpt-4",
         messages: [{
           role: "user",
           content: chatMessage
